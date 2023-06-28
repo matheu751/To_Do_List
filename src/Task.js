434719249def4f7411e50773fbb159a512a01fa3
mps751 <you@example.com>
2023-06-28 13:09:19 -0300

    adjusting COMPONENT

---
+++ src/Task.js
@@ -0,0 +1,10 @@
+export const Task = (props) => {
+  return (
+    <div className="task" 
+    style={{backgroundColor: props.completed ? "green" : "white"}}>
+      <h1> {props.taskName} </h1>
+      <button onClick={() => props.completedTask(props.id)}> Completed </button>
+      <button onClick={() => props.deleteTask(props.id)}> x </button>
+    </div>
+  )
+}
